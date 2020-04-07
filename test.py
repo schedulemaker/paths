a2d6a9ca018af391c437b6a7b3a8f0d5a69462c7
ericgurevich <tug00319@temple.edu>
2020-04-07 00:20:02 -0400

    sort schedules by meeting time

--- test.py
+++ test.py
@@ -60,4 +60,297 @@ def testBingMaps():
 
     print(getCampuses())
 
-print(getCampuses())
+def sortedMeetingTimes(schedule):
+    days = {'monday': [], 
+    'tuesday': [], 
+    'wednesday':[], 
+    'thursday': [], 
+    'friday': [], 
+    'saturday':[], 
+    'sunday': []}
+    
+
+    for course in schedule:
+        for meetingTime in course['meetingTimes']:
+            if meetingTime['monday']:
+                days['monday'].append(meetingTime)
+            if meetingTime['tuesday']:
+                days['tuesday'].append(meetingTime)
+            if meetingTime['wednesday']:
+                days['wednesday'].append(meetingTime)
+            if meetingTime['thursday']:
+                days['thursday'].append(meetingTime)
+            if meetingTime['friday']:
+                days['friday'].append(meetingTime)
+            if meetingTime['saturday']:
+                days['saturday'].append(meetingTime)
+            if meetingTime['sunday']:
+                days['sunday'].append(meetingTime)
+            
+    for day in days.keys():
+        days[day] = sorted(days[day], key=lambda meetingTime: meetingTime['startTime'])
+
+    return days
+
+schedule = [
+      {
+         "isOpen":True,
+         "campus":"CC",
+         "meetingTimes":[
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":False,
+               "building":"TUCC",
+               "room":"00315",
+               "instructors":[
+
+               ],
+               "sunday":False,
+               "tuesday":True,
+               "wednesday":False,
+               "friday":False,
+               "startTime":1730,
+               "endTime":2050,
+               "monday":False
+            }
+         ],
+         "courseName":"FMA-5671",
+         "title":"Film History and Theory",
+         "crn":44774
+      },
+      {
+         "isOpen":True,
+         "campus":"MN",
+         "meetingTimes":[
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":False,
+               "building":"SERC",
+               "room":"00357",
+               "instructors":[
+                  "Eugene Kwatny"
+               ],
+               "sunday":False,
+               "tuesday":False,
+               "wednesday":True,
+               "friday":False,
+               "startTime":900,
+               "endTime":1050,
+               "monday":False
+            },
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":True,
+               "building":"BEURY",
+               "room":"00164",
+               "instructors":[
+                  "Eugene Kwatny"
+               ],
+               "sunday":False,
+               "tuesday":True,
+               "wednesday":False,
+               "friday":False,
+               "startTime":1230,
+               "endTime":1350,
+               "monday":False
+            }
+         ],
+         "courseName":"CIS-3207",
+         "title":"Introduction to Systems Programming and Operating Systems",
+         "crn":4308
+      },
+      {
+         "campus":"MN",
+         "meetingTimes":[
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":False,
+               "building":None,
+               "room":None,
+               "instructors":[
+
+               ],
+               "sunday":False,
+               "tuesday":False,
+               "wednesday":False,
+               "friday":False,
+               "startTime":0,
+               "endTime":0,
+               "monday":False
+            }
+         ],
+         "isOpen":True,
+         "attributes":{
+            "description":"GenEd Race &amp; Diversity",
+            "isZTCAttribute":False,
+            "code":"GD",
+            "class":"net.hedtech.banner.student.schedule.SectionDegreeProgramAttributeDecorator",
+            "courseReferenceNumber":"25550",
+            "termCode":"202036"
+         },
+         "courseName":"ANTH-0831",
+         "title":"Immigration and the American Dream",
+         "crn":25550
+      },
+      {
+         "campus":"MN",
+         "meetingTimes":[
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":True,
+               "building":"ANDRSN",
+               "room":"00017",
+               "instructors":[
+                  "Kyle Harris",
+                  "Kyle Suess"
+               ],
+               "sunday":False,
+               "tuesday":True,
+               "wednesday":False,
+               "friday":False,
+               "startTime":1400,
+               "endTime":1520,
+               "monday":False
+            },
+            {
+               "saturday":False,
+               "weeks":[
+                  1,
+                  2,
+                  3,
+                  4,
+                  5,
+                  6,
+                  7,
+                  8,
+                  9,
+                  10,
+                  11,
+                  12,
+                  13,
+                  14,
+                  15,
+                  16
+               ],
+               "thursday":True,
+               "building":"PEARMC",
+               "room":"0P015",
+               "instructors":[
+                  "Kyle Harris",
+                  "Kyle Suess"
+               ],
+               "sunday":False,
+               "tuesday":False,
+               "wednesday":False,
+               "friday":False,
+               "startTime":1730,
+               "endTime":1920,
+               "monday":False
+            }
+         ],
+         "isOpen":True,
+         "attributes":{
+            "description":"_Core Science &amp; Technology A",
+            "isZTCAttribute":False,
+            "code":"SA",
+            "class":"net.hedtech.banner.student.schedule.SectionDegreeProgramAttributeDecorator",
+            "courseReferenceNumber":"2184",
+            "termCode":"202036"
+         },
+         "courseName":"KINS-1223",
+         "title":"Human Anatomy and Physiology I",
+         "crn":2184
+      }
+   ]
+
+print(sortedMeetingTimes(schedule))
