291f73f677ed461af0dfbedd892a3d247bb02349
ericgurevich <tug00319@temple.edu>
2020-04-06 22:53:21 -0400

    get campuses

--- test.py
+++ test.py
@@ -1,18 +1,52 @@
 import requests
 import json
+from requests_aws4auth import AWS4Auth
+import boto3
 
-def getAPIKey():
+def getCampuses():
+    session = requests.Session()
+
+    APPSYNC_API_ENDPOINT_URL = 'https://otymenn5hncylox27rmmlwodcy.appsync-api.us-east-2.amazonaws.com/graphql'
+
+    query = """query GetCampus( 
+    $event: BannerProxyInput = {
+        school: "temple"
+        term: 202036
+        method: "getCampus"
+    }
+    ) {
+    getBannerMetadata(event: $event) {
+        code
+        description
+    }
+    }"""
+
+    response = session.request(
+        url = APPSYNC_API_ENDPOINT_URL,
+        method = 'POST',
+        headers= {'x-api-key': getAPIKey('appsync_key')},
+        json= {'query': query}
+    )
+
+    campusDict = {}
+
+    for i in response.json()['data']['getBannerMetadata']:
+        campusDict[i['code']] = i['description']
+
+    return campusDict
+
+def getAPIKey(key):
     with open('api_key.json') as f:
         data = json.load(f)
 
-    return data['key']
+    return data[key]
 
-def test():
+def testBingMaps():
     url = 'http://dev.virtualearth.net/REST/v1/Routes/Driving'
 
     params = {'waypoint.1': '{{temple university tuttleman learning center}}',
             'waypoint.2': '{{temple ambler campus}}',
-            'key': getAPIKey(),
+            'key': getAPIKey('bing_key'),
             'distanceUnit': 'mi'
             }
 
@@ -24,4 +58,6 @@ def test():
     print(result['resourceSets'][0]["resources"][0]["travelDuration"])
     print(result['resourceSets'][0]["resources"][0]["travelDurationTraffic"])
 
-test()
+    print(getCampuses())
+
+print(getCampuses())
